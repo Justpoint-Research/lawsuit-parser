074f05a06eb139ddb4f74e121ec1589c83747ddf
Krzysztof Suwada <krzysztof.suwada@justpoint.com>
2026-08-17 13:50:26 +0200

    Add PdfBackend.DLPARSE_V2 to enable GPU acceleration

--- lawsuit_parser/parsers/pdf_parser.py
+++ lawsuit_parser/parsers/pdf_parser.py
@@ -7,7 +7,7 @@ from typing import Any
 
 from docling.document_converter import DocumentConverter
 from docling.datamodel.base_models import InputFormat
-from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling.datamodel.pipeline_options import PdfPipelineOptions, PdfBackend
 from docling.datamodel.accelerator_options import AcceleratorOptions, AcceleratorDevice
 from docling_core.types.doc import DocItemLabel
 from docling_core.types.doc.base import ImageRefMode
@@ -99,7 +99,9 @@ def parse_pdf_document(
     )
 
     # Configure PDF pipeline options
+    # Use DLPARSE_V2 backend which supports GPU acceleration
     pipeline_options = PdfPipelineOptions(
+        backend=PdfBackend.DLPARSE_V2,
         accelerator_options=accelerator_options,
         do_table_structure=extract_tables,
         do_ocr=True,
